f7b6b9b1acf0c00cf1bb053750f082f29af86ed3
Juan hofman <juanhofman@outlook.com>
2020-03-27 01:34:48 +0100

    Init VS

---
+++ EpicEvents/EpicEvents/Main.cs
@@ -0,0 +1,28 @@
+﻿using LSPD_First_Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rage;
+using Rage.Native;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting.Entities;
+
+namespace EpicEvents
+{
+    public class Main : Plugin
+    {
+        public override void Initialize()
+        {
+            
+        }
+
+        public override void Finally()
+        {
+            
+        }
+
+
+    }
+}
